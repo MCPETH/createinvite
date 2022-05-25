c11cf814a1589ba0812b526c8c244f3dc7d3d93d
MCPETH <thtongame@gmail.com>
2022-05-25 14:44:48 +0700

    Update example.js

--- example.js
+++ example.js
@@ -1,3 +1,6 @@
+const Discord = require('discord.js');
+const client = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES] });
+
 client.on('ready', () => {
 	// Create an invite to a channel
 	channel = client.guilds.cache.get("enter your guilds id").channels.cache.get("enter your channels id");
@@ -5,3 +8,5 @@ client.on('ready', () => {
 		.then(invite => console.log(`Created an invite with a code of ${invite.code}`))
 		.catch(console.error);
 })
+
+client.login('your Discord bot token');
